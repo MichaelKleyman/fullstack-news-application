5bdc9c145492122f3762140bc879ffcdc9c8eef8
Michael Kleyman <michaelkleyman99@gmail.com>
2023-04-06 11:56:20 -0400

    made the home page a server component instead of a client component which made the api calls function properly

--- Practice-projects/news-application/client/app/articles/[slug]/not-found.js
+++ Practice-projects/news-application/client/app/articles/[slug]/not-found.js
@@ -1,15 +1,25 @@
 'use client';
 import { forwardRef } from 'react';
 import MuiAlert from '@mui/material/Alert';
+import { useRouter } from 'next/navigation';
 
 const Alert = forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />;
 });
 
 const NotFound = () => {
+  const router = useRouter();
+
   return (
-    <div>
+    <div className='m-6'>
       <Alert severity='error'>Article Not Found</Alert>
+      <p
+        type='button'
+        onClick={() => router.push('/')}
+        className='cursor-pointer hover:font-bold duration-300 p-4'
+      >
+        Go back
+      </p>
     </div>
   );
 };
--- Practice-projects/news-application/client/app/page.js
+++ Practice-projects/news-application/client/app/page.js
@@ -1,6 +1,4 @@
-'use client';
 import Axios from 'axios';
-import { useState, useEffect } from 'react';
 import Page from '../components/page';
 
 const URL = 'http://localhost:3001/newsapp/articles';
@@ -8,6 +6,7 @@ const URL = 'http://localhost:3001/newsapp/articles';
 //should be server side rendering, like getServerSideProps
 async function fetchArticles() {
   const response = await Axios.get(URL);
+  console.log(response);
   if (response) {
     return response.data;
   } else {
@@ -15,26 +14,25 @@ async function fetchArticles() {
   }
 }
 
-const page = () => {
-  const [articles, setArticles] = useState([]);
+export default async function page() {
+  // const [articles, setArticles] = useState([]);
 
-  useEffect(() => {
-    async function backendCall() {
-      const data = await fetchArticles();
-      if (data) {
-        setArticles(data);
-      } else {
-        console.log('Error');
-      }
-    }
-    backendCall();
-  }, []);
+  // useEffect(() => {
+  //   async function backendCall() {
+  //     const data = await fetchArticles();
+  //     if (data) {
+  //       setArticles(data);
+  //     } else {
+  //       console.log('Error');
+  //     }
+  //   }
+  //   backendCall();
+  // }, []);
+  const articles = await fetchArticles();
 
   return (
     <div className='p-4'>
       <Page articles={articles} />
     </div>
   );
-};
-
-export default page;
+}
