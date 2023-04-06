68e00fbb4256130b078d09f7e565bc400b2cf4d0
Michael Kleyman <michaelkleyman99@gmail.com>
2023-04-06 13:48:55 -0400

    setup proper loading state for when the server may be slow when sending data

---
+++ Practice-projects/news-application/client/app/loading.js
@@ -0,0 +1,10 @@
+'use client';
+import CircularProgress from '@mui/material/CircularProgress';
+
+export default function CircularIndeterminate() {
+  return (
+    <div className='flex items-center justify-center mt-[20rem]'>
+      <CircularProgress />
+    </div>
+  );
+}
