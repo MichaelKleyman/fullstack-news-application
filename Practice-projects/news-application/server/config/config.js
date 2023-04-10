ea6c06334c5bc1b29333aecdd0cae0a38efa33d7
Michael Kleyman <michaelkleyman99@gmail.com>
2023-04-10 07:36:53 -0400

    successfuly setup Articles model via sequelize, and ran the migrations to create the table and hook it up to the database

--- Practice-projects/news-application/server/config/config.js
+++ Practice-projects/news-application/server/config/config.js
@@ -1,7 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
   development: {
     username: 'michaelkleyman',
-    password: null,
+    password: process.env.DATABASE_PASSWORD,
     database: 'News-App',
     host: '127.0.0.1',
     dialect: 'postgres',
