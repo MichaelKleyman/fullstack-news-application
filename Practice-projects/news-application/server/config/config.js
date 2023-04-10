eb09cfaadbf5749b3ba3536c776f2473b97bd51e
Michael Kleyman <michaelkleyman99@gmail.com>
2023-04-10 07:10:05 -0400

    changed backend config file from json to js

---
+++ Practice-projects/news-application/server/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: 'michaelkleyman',
+    password: null,
+    database: 'News-App',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  test: {
+    username: 'michaelkleyman',
+    password: null,
+    database: 'News-App',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    username: 'michaelkleyman',
+    password: null,
+    database: 'News-App',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+};
